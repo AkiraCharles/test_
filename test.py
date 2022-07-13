a02b63ff3784e5c30a2c2e5ca20692a176f71b9b
AkiraCharles <charles.akira@gmail.com>
2022-07-13 13:09:02 -0400

    Branch Update

--- test.py
+++ test.py
@@ -3,3 +3,13 @@ import pygame
 print("Hello Git")
 
 print("Hopefully this works Git")
+
+print("Hey I am on the branch, maybe you will add me to main one day")
+
+a = 1
+
+b = 3
+
+c = a+ b
+
+print(c)
